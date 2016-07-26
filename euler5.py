a4ea73aaef69713011310f59634f9f999108d98e
tomhewletttaylor <tom.hewlett.taylor@googlemail.com>
2016-07-26 16:23:18 +0100

    Add lowest_common_multiple_of_numbers_up_to and euler5 functions and associated tests for problem 5.

--- euler5.py
+++ euler5.py
@@ -18,3 +18,26 @@ def prime_numbers_up_to(number):
         candidate_prime += 1
     return [index + 1 for (index, prime) in enumerate(working_list) if prime]
 
+
+def lowest_common_multiple_of_numbers_up_to(number):
+    """Return the lowest common multiple of all numbers up to and including a particular number.
+
+    Args:
+        number: The number to find the LCM of all numbers up to.
+    Returns:
+        The LCM of all numbers up to and including number.
+    """
+    lcm = 1
+    primes_up_to_number = prime_numbers_up_to(number)
+    for prime in primes_up_to_number:
+        exponent = 1
+        prime_power = prime ** exponent
+        while prime_power <= number:
+            exponent += 1
+            prime_power = prime ** exponent
+        lcm *= prime ** (exponent - 1)
+    return lcm
+
+
+def euler5():
+    return lowest_common_multiple_of_numbers_up_to(20)
--- euler5tests.py
+++ euler5tests.py
@@ -5,3 +5,13 @@ import unittest
 class PrimeNumbersUpToTestCase(unittest.TestCase):
     def test_primes_up_to_10_are_2_3_5_and_7(self):
         self.assertEquals([2, 3, 5, 7], euler5.prime_numbers_up_to(10))
+
+
+class LowestCommonMultipleOfNumbersUpToTestCase(unittest.TestCase):
+    def test_lcm_of_numbers_up_to_10_is_2520(self):
+        self.assertEquals(2520, euler5.lowest_common_multiple_of_numbers_up_to(10))
+
+
+class Euler5TestCase(unittest.TestCase):
+    def test_euler5_returns_232792560(self):
+        self.assertEquals(232792560, euler5.euler5())
