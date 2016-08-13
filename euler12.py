ce493716f6e583e4f0f3389d12d3bdaf363be994
Tom Hewlett-Taylor <tom.hewlett.taylor@googlemail.com>
2016-08-13 13:39:01 +0100

    Add unimplemented number_of_factors and first_triangle_number_with_number_of_factors functions and failing unit tests for problem 12.

--- euler12.py
+++ euler12.py
@@ -1,2 +1,10 @@
+def number_of_factors(number):
+    return 0
+
+
+def first_triangle_number_with_number_of_factors(factors):
+    return 0
+
+
 def euler12():
     return 0
--- euler12tests.py
+++ euler12tests.py
@@ -2,6 +2,19 @@ import euler12
 import unittest
 
 
+class NumberOfFactorsTestCase(unittest.TestCase):
+    def test_6_has_4_factors(self):
+        self.assertEquals(4, euler12.number_of_factors(6))
+
+    def test_32_has_5_factors(self):
+        self.assertEquals(5, euler12.number_of_factors(32))
+
+
+class FirstTriangleNumberWithNumberOfFactorsTestCase(unittest.TestCase):
+    def test_first_triangle_number_with_5_factors_is_28(self):
+        self.assertEquals(28, euler12.first_triangle_number_with_number_of_factors(5))
+
+
 class Euler12TestCase(unittest.TestCase):
     def test_euler12_returns_76576500(self):
         self.assertEquals(76576500, euler12.euler12())
