83e47466daef438ce1dfa68edf970afeee62e99c
Tom Hewlett-Taylor <tom.hewlett.taylor@googlemail.com>
2016-08-13 14:02:09 +0100

    Add implementation of number_of_factors function for problem 12.

--- euler12.py
+++ euler12.py
@@ -1,5 +1,19 @@
-def number_of_factors(number):
-    return 0
+import shared
+
+
+def number_of_factors(number, primes=None):
+    if primes is None:
+        primes = shared.prime_numbers_up_to(number)
+    factors = 1
+    for prime in primes:
+        if number == 1:
+            break
+        exponent = 0
+        while number % prime == 0:
+            number = number / prime
+            exponent += 1
+        factors *= (exponent + 1)
+    return factors
 
 
 def first_triangle_number_with_number_of_factors(factors):
